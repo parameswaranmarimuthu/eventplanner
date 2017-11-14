ceb01302f3fd90c07cd543e9076253737f701ef0
Parameswaran Marimuthu <parameswaran.marimuthu@macys.com>
2017-11-14 14:40:28 -0800

    added the service and model classes

---
+++ src/main/java/com/yelloweclips/eventplanner/model/User.java
@@ -0,0 +1,8 @@
+package com.yelloweclips.eventplanner.model;
+
+/**
+ * Created by m655222 on 11/14/2017.
+ */
+public class User {
+
+}
---
+++ src/main/java/com/yelloweclips/eventplanner/service/EventPlannerService.java
@@ -0,0 +1,8 @@
+package com.yelloweclips.eventplanner.service;
+
+/**
+ * Created by m655222 on 11/14/2017.
+ */
+public class EventPlannerService {
+    
+}
