cda3fba99e195fc4e6b37601387b62d43a18467f
Parameswaran Marimuthu <parameswaranmarimuthu@PM.local>
2017-11-14 19:08:03 -0800

    adding spring data with mongodb

--- src/main/java/com/yelloweclips/eventplanner/model/Event.java
+++ src/main/java/com/yelloweclips/eventplanner/model/Event.java
@@ -1,8 +1,35 @@
 package com.yelloweclips.eventplanner.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  * Created by m655222 on 11/14/2017.
  */
+
+@Document(collection = "events")
 public class Event {
+
+    @Id
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
 
 }
---
+++ src/main/java/com/yelloweclips/eventplanner/service/MongoEventRepository.java
@@ -0,0 +1,13 @@
+package com.yelloweclips.eventplanner.service;
+
+import com.yelloweclips.eventplanner.model.Event;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+
+public interface MongoEventRepository extends MongoRepository<Event, Long> {
+
+    
+
+
+}
